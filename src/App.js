f5e4c620abe257caeddc60162b88a052735ad2df
Ryan Silva <ryandotsilva@gmail.com>
2019-04-06 15:16:12 -0400

    Add state to App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working</p>
-        <button>Switch Name</button>
-        <Person name="Max" age="28" />
-        <Person name="Manu" age="29">My Hobbies: Racing</Person>
-        <Person name="Stephanie" age="26"/>
-      </div>
-    );
+	state = {
+		persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 26 }
+    ]
+	};
 
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
-  }
+	render() {
+		return (
+			<div className="App">
+				<h1>Hi, I'm a React App</h1>
+				<p>This is really working</p>
+				<button>Switch Name</button>
+				<Person name="Max" age="28" />
+				<Person name="Manu" age="29">
+					My Hobbies: Racing
+				</Person>
+				<Person name="Stephanie" age="26" />
+			</div>
+		);
+
+		// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+	}
 }
 
 export default App;

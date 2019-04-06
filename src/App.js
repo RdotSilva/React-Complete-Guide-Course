933aac0f2430ea08e74403a3a551914246121788
Ryan Silva <ryandotsilva@gmail.com>
2019-04-06 18:45:41 -0400

    Move Persons to if statement above return

--- src/App.js
+++ src/App.js
@@ -47,17 +47,13 @@ class App extends Component {
 			border: "1px solid blue",
 			padding: "8px",
 			cursor: "pointer"
-		};
+    };
+    
+    let persons = null;
 
-		return (
-			<div className="App">
-				<h1>Hi, I'm a React App</h1>
-				<p>This is really working!</p>
-				<button style={style} onClick={this.togglePersonsHandler}>
-					Switch Name
-				</button>
-				{this.state.showPersons === true ? (
-					<div>
+    if (this.state.showPersons) {
+      persons = (
+        <div>
 						<Person
 							name={this.state.persons[0].name}
 							age={this.state.persons[0].age}
@@ -75,7 +71,17 @@ class App extends Component {
 							age={this.state.persons[2].age}
 						/>
 					</div>
-				) : null}
+      );
+    }
+
+		return (
+			<div className="App">
+				<h1>Hi, I'm a React App</h1>
+				<p>This is really working!</p>
+				<button style={style} onClick={this.togglePersonsHandler}>
+					Toggle Persons
+				</button>
+        {persons}
 			</div>
 		);
 		// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));

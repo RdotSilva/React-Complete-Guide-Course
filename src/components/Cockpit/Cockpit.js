63f34da782e4fea5bfb4047940a59eb891968c07
Ryan Silva <ryandotsilva@gmail.com>
2019-04-07 18:53:36 -0400

    Wrap Person return in Aux

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -5,7 +5,7 @@ const cockpit = props => {
 	useEffect(() => {
 		console.log('[Cockpit.js] useEffect');
 		// Http request...
-		const timer = setTimeout(() => {
+		setTimeout(() => {
 			alert('Saved data to cloud!');
 		}, 1000);
 		return () => {

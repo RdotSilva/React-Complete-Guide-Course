91b18bb374850f666757b34cbe5f55a7a29e1a70
Ryan Silva <ryandotsilva@gmail.com>
2019-04-06 20:11:45 -0400

    Update nameChangedHandler method

--- src/App.js
+++ src/App.js
@@ -5,22 +5,29 @@ import Person from "./Person/Person";
 class App extends Component {
 	state = {
 		persons: [
-			{ name: "Max", age: 28 },
-			{ name: "Manu", age: 29 },
-			{ name: "Stephanie", age: 26 }
+			{ id: "tvf3", name: "Max", age: 28 },
+			{ id: "t5f3", name: "Manu", age: 29 },
+			{ id: "t1f3", name: "Stephanie", age: 26 }
 		],
 		otherState: "some other value",
 		showPersons: false
 	};
 
-	nameChangedHandler = event => {
-		this.setState({
-			persons: [
-				{ id: 't2', name: "Max", age: 28 },
-				{ id: '3t', name: event.target.value, age: 29 },
-				{ id: 't4', name: "Stephanie", age: 27 }
-			]
+	nameChangedHandler = (event, id) => {
+		const personIndex = this.state.persons.findIndex(p => {
+			return p.id === id;
 		});
+
+		const person = {
+			...this.state.persons[personIndex]
+		};
+
+		person.name = event.target.value;
+
+		const persons = [...this.state.persons];
+		persons[personIndex] = person;
+
+		this.setState({ persons: persons });
 	};
 
 	deletePersonHandler = personIndex => {
@@ -53,8 +60,9 @@ class App extends Component {
 							<Person
 								click={() => this.deletePersonHandler(index)}
 								name={person.name}
-                age={person.age}
-                key={index}
+								age={person.age}
+								key={index}
+								changed={event => this.nameChangedHandler(event, person.id)}
 							/>
 						);
 					})}

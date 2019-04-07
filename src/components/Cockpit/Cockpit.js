2a0ea464bdd3ea6efe624b52c4e5d87e7befb18d
Ryan Silva <ryandotsilva@gmail.com>
2019-04-07 17:56:48 -0400

    Add componentWillUnmount hook

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -4,7 +4,13 @@ import classes from './Cockpit.css';
 const cockpit = props => {
 	useEffect(() => {
 		console.log('[Cockpit.js] useEffect');
-	});
+		// Http request...
+		setTimeout(() => {
+			alert('Saved data to cloud!');
+		}, 1000);
+	}, []);
+
+	// useEffect();
 
 	const assignedClasses = [];
 	let btnClass = '';

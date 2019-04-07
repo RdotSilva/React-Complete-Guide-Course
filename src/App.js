69e5fda2c406be4d9a91405ab7013990d0acd4d8
Ryan Silva <ryandotsilva@gmail.com>
2019-04-07 13:23:04 -0400

    Wrap Person with ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -51,13 +51,14 @@ class App extends Component {
 				<div>
 					{this.state.persons.map((person, index) => {
 						return (
-							<Person
-								click={() => this.deletePersonHandler(index)}
-								name={person.name}
-								age={person.age}
-								key={index}
-								changed={event => this.nameChangedHandler(event, person.id)}
-							/>
+							<ErrorBoundary key={person.id}>
+								<Person
+									click={() => this.deletePersonHandler(index)}
+									name={person.name}
+									age={person.age}
+									changed={event => this.nameChangedHandler(event, person.id)}
+								/>
+							</ErrorBoundary>
 						);
 					})}
 				</div>

66cf437a32302f73052613b99868fd0be52b72f4
Ryan Silva <ryandotsilva@gmail.com>
2019-04-08 11:08:58 -0400

    Remove AuthContext

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -44,22 +44,17 @@ class Persons extends PureComponent {
 
 	render() {
 		console.log('[Persons.js] rendering...');
-		return (
-			<AuthContext.Consumer>
-				{this.props.persons.map((person, index) => {
-					return (
-						<Person
-							click={() => this.props.clicked(index)}
-							name={person.name}
-							age={person.age}
-							key={person.id}
-							changed={event => this.props.changed(event, person.id)}
-							isAuth={this.props.isAuthenticated}
-						/>
-					);
-				})}
-			</AuthContext.Consumer>
-		);
+		return this.props.persons.map((person, index) => {
+			return (
+				<Person
+					click={() => this.props.clicked(index)}
+					name={person.name}
+					age={person.age}
+					key={person.id}
+					changed={event => this.props.changed(event, person.id)}
+				/>
+			);
+		});
 	}
 }
 

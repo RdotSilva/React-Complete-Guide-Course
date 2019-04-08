9f63f58f475642a832ca452df6e8b65c3bd91946
Ryan Silva <ryandotsilva@gmail.com>
2019-04-08 11:13:44 -0400

    Add AuthContext.Consumer

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Aux from '../../../hoc/Auxiliary';
 import withClass from '../../../hoc/withClass';
 import classes from './Person.css';
-import AuthContext from '../../../context/auth-context;
+import AuthContext from '../../../context/auth-context';
 
 class Person extends Component {
 	constructor(props) {
@@ -20,7 +20,11 @@ class Person extends Component {
 		console.log('[Person.js] rendering...');
 		return (
 			<Aux>
-				{this.props.isAuth ? <p>Authenticated!</p> : <p>Please log in</p>}
+				<AuthContext.Consumer>
+					{context =>
+						context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>
+					}
+				</AuthContext.Consumer>
 				<p onClick={this.props.click}>
 					I'm {this.props.name} and I am {this.props.age} years old!
 				</p>

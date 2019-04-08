b680c6212e4d5f5b6f612b4d6aef9deb7701dacc
Ryan Silva <ryandotsilva@gmail.com>
2019-04-08 10:22:49 -0400

    Import PropTypes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Aux from '../../../hoc/Auxiliary';
 import withClass from '../../../hoc/withClass';
 import classes from './Person.css';

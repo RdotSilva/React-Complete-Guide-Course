4d949f6f113bf1497fc904d80d526650b44911bc
Ryan Silva <ryandotsilva@gmail.com>
2019-04-08 11:22:45 -0400

    Import useContext hook

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 import classes from './Cockpit.css';
 import AuthContext from '../../context/auth-context';
 
 const cockpit = props => {
 	const toggleBtnRef = useRef(null);
+	const authContext = useContext(AuthContext);
 
 	useEffect(() => {
 		console.log('[Cockpit.js] useEffect');
